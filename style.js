8245236132ada47ecf41080460d8c8b300f59761
zidaneathallah <zidanathallahwinata@gmail.com>
2023-11-22 10:12:56 +0700

    first commit

---
+++ style.js
@@ -0,0 +1,17 @@
+var typed = new Typed(".typing", {
+  strings: ["Zidane Athallah"],
+    typeSpeed: 80,
+    showCursor: false,
+});
+
+var typed = new Typed(".ketik", {
+  strings: ["Front End Developer"],
+  typeSpeed: 80,
+  showCursor: false,
+});
+
+var typed = new Typed(".write", {
+  strings: ["UI/UX Designer"],
+  typeSpeed: 80,
+  showCursor: false,
+});
